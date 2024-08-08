8a555c6abc23ec70455b9efeced0ebe7fa74382c
Dalton Menezes <daltonmenezes@outlook.com>
2024-08-08 19:40:51 -0300

    fix(mobile): by breaking words on code mdx component

--- src/components/docs/mdx-components/code.tsx
+++ src/components/docs/mdx-components/code.tsx
@@ -7,7 +7,7 @@ export const code = ({
   return (
     <code
       className={cn(
-        'relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm',
+        'break-words relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm',
         className
       )}
       {...props}
